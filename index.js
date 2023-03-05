eba2b84672b02aafe8aa2d1e8484189952075b69
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-03-05 14:56:58 -0500

    implemented coordinates fetch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const {fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('198.58.160.76', (error, coordinates) => {
+
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Here are the coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -1,24 +1,45 @@
-const fetchMyIP = function(callback) { 
+const request = require('request');
+
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
 
-  const request = require('request');
+  
 
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
       return callback(error, null);
     }
 
-      if (response.statusCode !== 200) {
-        const message = ` Status code ${response.statusCode} when fetching IP. Response: ${body}`;
-       return callback(Error(message), null);
-      }
-
-        const data = JSON.parse(body);
-        const ip = data.ip;
-        callback(null, ip);
-      
-    });
+    if (response.statusCode !== 200) {
+      const message = ` Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(message), null);
+    }
+    
+    const data = JSON.parse(body);
+    const ip = data.ip;
+    callback(null, ip);
+  });
   
-  }
+};
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+
+  request(`https://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    
+    const parsedData = JSON.parse(body);
+    
+    if (!parsedData.success) {
+      const message = `Success status was ${parsedData.success}. Server message says: ${parsedData.message} when fetching for IP ${parsedData.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    
+    const { latitude, longitude } = parsedData;
+    callback(null, {latitude, longitude});
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
